b0a9e9cbc89c8dc3b119f60fb9a2e606fbd96cd7
jgilyam <jgilyam@folcode.com>
2023-08-01 17:10:03 -0300

    se instancias los objetos necesarios para el func de la api

---
+++ src/file/file.dependencies.ts
@@ -0,0 +1,9 @@
+import { FileService } from "./application/file.service";
+import { FileController } from "./infrastructure/file.controller";
+import { FileErrorHandler } from "./infrastructure/file.error.handler.";
+import { S3Service } from "./infrastructure/s3/s3.service";
+
+const s3Service = new S3Service()
+const fileService = new FileService(s3Service);
+const fileErrorHandler = new FileErrorHandler();
+export const fileController = new FileController(fileService, fileErrorHandler);
