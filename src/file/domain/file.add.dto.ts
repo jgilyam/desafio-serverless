7ebcfad3aca0cc679d92ac472c623361b466c0cc
jgilyam <jgilyam@folcode.com>
2023-08-01 17:02:31 -0300

    se agrega dtos

---
+++ src/file/domain/file.add.dto.ts
@@ -0,0 +1,4 @@
+export interface FileAddDTO{
+    filename: string;
+    data: Buffer;
+}
---
+++ src/file/domain/file.out.dto.ts
@@ -0,0 +1,5 @@
+export interface FileOutDTO {
+    uploadDate: Date;
+    filename: string;
+    signedUrl: string;
+}
