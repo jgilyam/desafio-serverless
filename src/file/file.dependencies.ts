65448184efbd91ed254f7dc42a534fd5d950f231
jgilyam <jgilyam@folcode.com>
2023-08-04 00:10:05 -0300

    se inyecta el bucker por constrictor

--- src/file/file.dependencies.ts
+++ src/file/file.dependencies.ts
@@ -3,7 +3,8 @@ import { FileController } from "./infrastructure/file.controller";
 import { FileErrorHandler } from "./infrastructure/file.error.handler.";
 import { S3Service } from "./infrastructure/s3/s3.service";
 
-const s3Service = new S3Service()
+const BUCKET = process.env.BUCKET ?? "";
+const s3Service = new S3Service(BUCKET)
 const fileService = new FileService(s3Service);
 const fileErrorHandler = new FileErrorHandler();
 export const fileController = new FileController(fileService, fileErrorHandler);
